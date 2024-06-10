932cbaba2ad164550b5153a30225e581e9980fc0
Juneyuehsu <freebluebox@gmail.com>
2024-06-10 12:06:43 +0800

    add count

--- guess.py
+++ guess.py
@@ -5,14 +5,19 @@
 
 import random
 r = random.randint(1, 100)
+count = 0  #想把猜幾次建出
+
 while True:
+	count = count + 1 # 也可以寫成 "count += 1"
 	num = input ('guess: ')
 	num = int (num)
 	if num == r:
 		print ('you got it')
+		print ('this is', count, 'times')
 		break
 	elif num > r:
 		print  ('bigger than answer')
 	elif num < r:
 		print ('smaller than answer')
+	print ('this is', count, 'times')
 
